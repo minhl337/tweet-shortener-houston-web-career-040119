195dd60c26c84552271e1f8b52572c18e39f51f4
minh nguyen <imnotminh@gmail.com>
2019-03-31 17:32:13 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,10 @@
 # Write your code here.
 
-dictionary={
-  "hello" =>'hi',
-  {"to"=>2, "two"=>2, "too"  '2' 
-  "for, four" become '4'
-  'be' becomes 'b'
-  'you' becomes 'u'
-  "at" becomes "@" 
-  "and" becomes "&"
-}
+def dictionary
+  dictionary_hash={
+    
+  }
+end
 
 def word_substituter(string)
   
