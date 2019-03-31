0ae6159427966f9c2f82bf45c569f0efa3db403a
minh nguyen <imnotminh@gmail.com>
2019-03-31 17:34:48 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -7,9 +7,9 @@ def dictionary
     "two"   => "2", 
     "too"   => "2"', 
     "for"   => "4", 
-    "four"  => '4',
-    'be'    => 'b',
-    'you'   => 'u',
+    "four"  => "4",
+    'be'    => "b",
+    'you'   => "u",
     "at"    => "@",
     "and"   => "&"
   }
