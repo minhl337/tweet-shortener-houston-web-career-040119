033672b062b8e736164a3f928ec0b02e3ec14405
minh nguyen <imnotminh@gmail.com>
2019-03-31 17:23:27 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,10 +2,10 @@
 
 def dictionary
   "hello" becomes 'hi'
-"to, two, too" become '2' 
-"for, four" become '4'
-'be' becomes 'b'
-'you' becomes 'u'
-"at" becomes "@" 
-"and" becomes "&"
+  "to, two, too" become '2' 
+  "for, four" become '4'
+  'be' becomes 'b'
+  'you' becomes 'u'
+  "at" becomes "@" 
+  "and" becomes "&"
 end
