90fed362061647e2a4ad55f39480158220c8f5a3
minh nguyen <imnotminh@gmail.com>
2019-03-31 17:33:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,13 +2,16 @@
 
 def dictionary
   dictionary_hash={
-    "hello" becomes 'hi'
-    "to, two, too" become '2' 
-    "for, four" become '4'
-    'be' becomes 'b'
-    'you' becomes 'u'
-    "at" becomes "@" 
-    "and" becomes "&"
+    "hello" => 'hi',
+    "to"    => "2", 
+    "two"   => "2", 
+    "too"   => '2' 
+    "for" =>"4", 
+    "four" => '4'
+    'be' => 'b'
+    'you' => 'u'
+    "at" => "@" 
+    "and" => "&"
   }
 end
 
