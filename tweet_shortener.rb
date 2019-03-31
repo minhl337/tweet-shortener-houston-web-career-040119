c595f4e1a9501f5abb7c999df839b9faf681968b
minh nguyen <imnotminh@gmail.com>
2019-03-31 17:24:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 
 def dictionary
-"hello" becomes 'hi'
-"to, two, too" become '2' 
-"for, four" become '4'
-'be' becomes 'b'
-'you' becomes 'u'
-"at" becomes "@" 
-"and" becomes "&"
+  "hello" becomes 'hi'
+  "to, two, too" become '2' 
+  "for, four" become '4'
+  'be' becomes 'b'
+  'you' becomes 'u'
+  "at" becomes "@" 
+  "and" becomes "&"
 end
